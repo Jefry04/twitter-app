59898b2129b4c234159d136837d76179388a441d
Jefferson <jefry04@gmail.com>
2021-02-05 22:22:46 -0500

    send name and email

--- api/controllers/users/index.js
+++ api/controllers/users/index.js
@@ -1,113 +1,121 @@
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-let User = require("./../../models/users");
-const config = require("./../../../config");
-const response = require ("./../../lib/response");
-
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+let User = require('./../../models/users');
+const config = require('./../../../config');
+const response = require('./../../lib/response');
 
 const login = (req, res) => {
-    const { username, password } =  req.body;
-    
-    User.find({ username: username}, ["password"])
+  const { username, password } = req.body;
+
+  User.find({ username: username }, ['name', 'email', 'password'])
     .then((users) => {
-        const findUser = bcrypt.compareSync(password, users[0].password);
-        if (findUser) {
-            const id = users[0]._id;
-            const token = jwt.sign({ 
-                exp: config.jwtExp,
-                id, }, config.jwtKey);
-            res.status(200).json(response(true, [{token}]));
-        }else{
-            res.status(500).json(response(false, undefined, "Datos no válidos"));
-        }
+      const [user = {}] = users;
+      const findUser = bcrypt.compareSync(password, user.password);
+      if (findUser) {
+        const id = user._id;
+        const token = jwt.sign({ id }, config.jwtKey, {
+          expiresIn: config.jwtExp,
+        });
+        const { name, email } = user;
+        res
+          .status(200)
+          .json(response(true, [{ user: { name, email }, token }]));
+      } else {
+        res.status(200).json(response(false, undefined, 'Datos no válidos'));
+      }
     })
-    .catch((errr) => {
-        res.status(500).json(response(false, undefined, err));
+    .catch((err) => {
+      res.status(500).json(response(false, undefined, err));
     });
 };
 
-
 const read = async (req, res) => {
-    try{
-        const users = await User.find({}, ["name", "username"]);
-        res.json(response(true, users));
-    }catch(err){
-        res.json(response(false, undefined, err));
-    }
+  try {
+    const users = await User.find({}, ['name', 'username']);
+    res.json(response(true, users));
+  } catch (err) {
+    res.json(response(false, undefined, err));
+  }
 };
 
-const create = (req, res)=>{
+const create = (req, res) => {
+  const { name, username, password, email } = req.body;
 
-    const { name, username, password, passwordConfirmation, email } = req.body;
+  const saltRounds = bcrypt.genSaltSync(config.SALT);
+  const passwordHashed = bcrypt.hashSync(password, saltRounds);
 
-    const saltRounds = bcrypt.genSaltSync(config.SALT);
-    const passwordHashed = bcrypt.hashSync(password, saltRounds);
+  const user = {
+    name,
+    username,
+    email,
+    password: passwordHashed,
+  };
 
-    const user = {
-        name,
-        username,
-        email,
-        password: passwordHashed
-    };
-
-    User.find({ username: user.username})
+  User.find({ username: user.username })
     .then((users) => {
-        if (users.length > 0){
-            res.json(response(false, undefined, "Ya existe "))
-        }else{
-           const obj = new User(user);
-           obj.save()
-           .then((user) => {
-               res.json(response(true, [user]))
-           })
-           .catch((err) => {
-               res.json(response(false, undefined, err));
-           });
-        }
+      if (users.length > 0) {
+        res.json(
+          response(
+            false,
+            undefined,
+            'Ya existe un usuario con el mismo username'
+          )
+        );
+      } else {
+        const obj = new User(user);
+        obj
+          .save()
+          .then((user) => {
+            res.json(response(true, [user]));
+          })
+          .catch((err) => {
+            res.json(response(false, undefined, err));
+          });
+      }
     })
     .catch((err) => {
-        res.json(response(false, undefined, err));
+      res.json(response(false, undefined, err));
     });
 };
 
 const remove = (req, res) => {
-    const username = req.params.username;
+  const username = req.params.username;
 
-    User.remove({username: username})
+  User.remove({ username: username })
     .then(() => {
-        res.json(response(true, [{message: "Usuario eliminado"}]));
+      res.json(response(true, [{ message: 'Usuario eliminado' }]));
     })
-    .catch(() => {
-        res.json(response(false, undefined, err));
+    .catch((err) => {
+      res.json(response(false, undefined, err));
     });
 };
 
 const update = (req, res) => {
-    const username = req.params.username;
+  const username = req.params.username;
 
-    const user = {
-        name: req.body.name,
-        email: req.body.email,
-    };
+  const user = {
+    name: req.body.name,
+    email: req.body.email,
+  };
 
-    User.findOneAndUpdate({username: username}, user)
+  User.findOneAndUpdate({ username: username }, user)
     .then((user) => {
-        res.json(response(true, user));
+      res.json(response(true, user));
     })
     .catch((err) => {
-        res.json(response(false, undefined, err));
+      res.json(response(false, undefined, err));
     });
 };
 
 const readOne = (req, res) => {
-    const username = req.params.username;
-    User.find({username: username}, ["name", "username"])
+  const username = req.params.username;
+  User.find({ username: username }, ['name', 'username'])
     .then((users) => {
-        res.json(response(true, users));
+      res.json(response(true, users));
     })
     .catch((err) => {
-        res.json(response(false, undefined, err));
+      res.json(response(false, undefined, err));
     });
 };
 
-module.exports = {read, create, remove, update, readOne, login}; 
+module.exports = { read, create, remove, update, readOne, login };
--- config/index.js
+++ config/index.js
@@ -1,16 +1,16 @@
 const config = {
-    port : process.env.PORT,
-    salt : process.env.SALT,
-    weatherKey : process.env.WEATHER_KEY,
-    jwtKey : process.env.JWT_KEY,
-    jwtExp: Math.floor(Date.now() / 1000) + (60 * 60),
-    dbConnectionString: process.env.DB_CONNECTION_STRING,
-    twitter: {
-        consumerKey: process.env.CONSUMER_KEY,
-        consumerSecret: process.env.CONSUMER_SECRET,
-        accessTokenKey: process.env.ACCESS_TOKEN_KEY,
-        accessTokenSecret: process.env.ACCESS_TOKEN_SECRET
-    }
-}
+  port: process.env.PORT,
+  salt: process.env.SALT,
+  weatherKey: process.env.WEATHER_KEY,
+  jwtKey: process.env.JWT_KEY,
+  jwtExp: '1h',
+  dbConnectionString: process.env.DB_CONNECTION_STRING,
+  twitter: {
+    consumerKey: process.env.CONSUMER_KEY,
+    consumerSecret: process.env.CONSUMER_SECRET,
+    accessTokenKey: process.env.ACCESS_TOKEN_KEY,
+    accessTokenSecret: process.env.ACCESS_TOKEN_SECRET,
+  },
+};
 
 module.exports = config;
